ea824fb4b1032ea99487b19d169dfd661c8cf5f9
abdelrahman <abdelrahmanalgendy97@gmail.com>
2022-11-07 23:41:45 +0200

    first

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from blog.models import Tag, Post , Comment 
+
+admin.site.register(Tag)
+
+admin.site.register(Comment)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('slug', 'published_at')
+
+
+admin.site.register(Post, PostAdmin)
